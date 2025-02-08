cac9b04554973c42a520eda5069326d9ce29c9ef
Seli  Chaaya Alachkar <selim.chaayaalachkar@iu-study.org>
2025-02-08 19:23:09 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+import { Link } from "@heroui/link";
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+import { button as buttonStyles } from "@heroui/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+export default function Home() {
+  return (
+    <div className="bg-gradient-to-tr from-red-500 to-purple-400 relative h-screen">
+
+
+    <div className="absolute inset-0 flex flex-col justify-center items-center w-5/6 max-w-lg mx-auto text-center">
+      
+      
+        <div className="space-y-8">
+          <h1 className="font-primary font-extrabold text-white text-3xl sm:text-4xl md:text-5xl md:leading-tight">
+          JARURAT CARE
+          <span className="text-palette-primary">
+            </span>
+          </h1>
+          <p className="font-secondary text-palette-light text-base md:text-lg lg:text-xl">
+          Providing support, guidance, hope and personalized care for cancer patients and their families. Here to ensure you never face your journey alone.          </p>
+        </div>
+  
+    </div>
+  </div>
+    );
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,72 @@
+"use client"; 
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from "@heroui/navbar";
+import { Button } from "@heroui/button";
+import { Link } from "@heroui/link";
+import { usePathname  } from "next/navigation"; 
+import { color } from "framer-motion";
+
+export const NGOLogo = () => {
+  return (
+    <Link color="foreground" href="/">
+      <img src="JC-logo.png" alt="JC Logo" className="responsiveLogo" />
+    </Link>
+  );
+};
+
+export default function App() {
+  const router = usePathname (); 
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  return (
+    <Navbar className="bgt" onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle 
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden links"
+        />
+        <NavbarBrand>
+          <NGOLogo />
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem isActive={router === "/"}>
+          <Link className={router === "/" ? undefined : "links"} href="/" color={router === "/" ? undefined : "foreground"}>
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className={router === "/contact" ? undefined : "links"} href="/contact" color={router === "/contact" ? undefined : "foreground"}>Contact</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className={router === "/int" ? undefined : "links"} color={router === "/int" ? undefined : "foreground"} href="/int">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button as={Link} href="/find-a-hospital" variant="flat">
+            Find A Hospital
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        <NavbarMenuItem>
+          <Link className={router === "/" ? "linksm" : undefined} href="#" size="lg">
+            Home
+          </Link>
+        </NavbarMenuItem>
+      </NavbarMenu>
+    </Navbar>
+  );
+}
